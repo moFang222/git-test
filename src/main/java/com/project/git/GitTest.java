81d743a5b29a8f0a2b85924890b2ce31a69ecd3d
JianqingWeng <826732756@qq.com>
2021-07-09 19:41:30 +0800

    first commit

---
+++ src/main/java/com/project/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.project.git;
+
+/**
+ * @author JianqingWeng
+ * @Description
+ * @create 2021-07-09 19:35
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello, Git!");
+    }
+}
